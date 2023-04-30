bd531f4992f3db1a8b4a8c3d3f0d4b218a0a5027
Kirby B <kirby.browning718@gmail.com>
2023-04-30 01:42:41 -0400

    forgot the last step in the Google solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 // import { Nav } from "../../components";
 // import { Footer } from "../../components";
 import { Home } from "./containers";
@@ -8,14 +8,14 @@ export const App = () => {
   return (
     <div className="App">
       {/* <Nav /> */}
-      <Routes>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Route path="/" element={<Home />} />
         {/* <Route path="/about" element={<About />} /> */}
         {/* <Route path="/contact" element={<Contact />} />
         <Route path="/training" element={<Training />} />
         <Route path="/video-gallery" element={<VideoGallery />} /> */}
         {/* <Route component={<Error />} /> */}
-      </Routes>
+      </HashRouter>
       {/* <Footer /> */}
     </div>
   );
