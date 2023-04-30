ad2e9a3c7127f1e412518ac5fe0c11a97755ca16
Kirby B <kirby.browning718@gmail.com>
2023-04-30 01:45:11 -0400

    undoing all of that because it wasn't what I wanted... yay

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 // import { Nav } from "../../components";
 // import { Footer } from "../../components";
 import { Home } from "./containers";
@@ -8,14 +8,14 @@ export const App = () => {
   return (
     <div className="App">
       {/* <Nav /> */}
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/about" element={<About />} /> */}
         {/* <Route path="/contact" element={<Contact />} />
         <Route path="/training" element={<Training />} />
         <Route path="/video-gallery" element={<VideoGallery />} /> */}
         {/* <Route component={<Error />} /> */}
-      </HashRouter>
+      </Routes>
       {/* <Footer /> */}
     </div>
   );
