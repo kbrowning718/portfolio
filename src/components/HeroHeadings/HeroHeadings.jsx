9b166b664ae0e9daa6c6ae0eda4b1888bea59f31
Kirby B <kirby.browning718@gmail.com>
2023-08-03 19:33:24 -0400

    cleaned up #hero1 css

--- src/components/HeroHeadings/HeroHeadings.jsx
+++ src/components/HeroHeadings/HeroHeadings.jsx
@@ -2,12 +2,10 @@ import "./HeroHeadings.css";
 
 export const HeroHeadings = ({ headerText, subheaderText, children }) => {
   return (
-    <div className="headline-wrapper">
-      <div className="text-wrapper">
-        <h1 className="hero-header">{headerText}</h1>
-        <p className="hero-subhead">{subheaderText}</p>
-        {children}
-      </div>
+    <div className="text-wrapper">
+      <h1 className="hero-header">{headerText}</h1>
+      <p className="hero-subhead">{subheaderText}</p>
+      {children}
     </div>
   );
 };
